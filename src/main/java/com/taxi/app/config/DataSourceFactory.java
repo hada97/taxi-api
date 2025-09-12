eb5c72af233039473f732c99c4af31abdb5f74ba
Adailson Guimaraes <99360493+hada97@users.noreply.github.com>
2025-09-12 01:11:41 -0300

    add hikari config

---
+++ src/main/java/com/taxi/app/config/DataSourceFactory.java
@@ -0,0 +1,39 @@
+package com.taxi.app.config;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class DataSourceFactory {
+
+    private static final HikariDataSource dataSource;
+
+    static {
+        HikariConfig config = new HikariConfig();
+
+        // H2 arquivo + Hikari
+        config.setJdbcUrl("jdbc:h2:file:./data/testdb;DB_CLOSE_DELAY=-1;AUTO_SERVER=TRUE");
+        config.setUsername("sa");
+        config.setPassword("");
+        config.setDriverClassName("org.h2.Driver");
+
+        // Configurações do Hikari
+        config.setMaximumPoolSize(5);
+        config.setMinimumIdle(1);
+        config.setIdleTimeout(30000);
+        config.setConnectionTimeout(20000);
+        config.setMaxLifetime(1800000);
+
+        dataSource = new HikariDataSource(config);
+    }
+
+    public static DataSource getDataSource() {
+        return dataSource;
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return dataSource.getConnection();
+    }
+}
